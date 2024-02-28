3568da63649c43f2e8cb3ee0c16ce40bc06fba43
Aqsa khan <161198603+Aqlish@users.noreply.github.com>
2024-02-28 16:05:52 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-var personalName = "aqsa khan";
-console.log("hello", personalName, "would you like to learn some phython today?");
+// lower case ;
+var personName = "Aqsa khan";
+console.log("lowercase", personName.tolowerCase, {});
+// upper case
+console.log("uppercase:", personName.tolowerCase, {});
+// Title case
+console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
--- main.ts
+++ main.ts
@@ -1,2 +1,9 @@
-let personalName = "aqsa khan";
-console.log("hello",personalName, "would you like to learn some phython today?");
+// lower case ;
+let personName: string ="Aqsa khan"
+console.log("lowercase", personName.tolowerCase{});
+
+// upper case
+console.log("uppercase:", personName.tolowerCase{});
+
+// Title case
+console.log("titlecase:", personName.replace(/\bw/g,c => c.toUpperCase()));
